662b78a2f993d1ff8ff98f15d38e746a8c8fc09f
Shubham Pathak <pathakshubham28@gmail.com>
2016-02-11 16:49:28 +0530

    Header based authorization and Logging

--- WevServiceFramework/SoapServiceTomcat/src/main/java/com/nostalgiaguy/soapservicetomcat/TestMethod.java
+++ WevServiceFramework/SoapServiceTomcat/src/main/java/com/nostalgiaguy/soapservicetomcat/TestMethod.java
@@ -1,18 +1,24 @@
 package com.nostalgiaguy.soapservicetomcat;
 
+
 import java.util.List;
 
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 @WebService
 public interface TestMethod {
+		
+	@WebResult(name="AdditionResult")
+	String addition(@WebParam(name="IntegerInput_1")int a,@WebParam(name="IntegerInput_2")int b);
 	
-	//int multiply(int a,int b);
+	@WebResult(name="SubtractionResult")
+	int subtraction(@WebParam(name="IntegerInput_1")int a,@WebParam(name="IntegerInput_2")int b);
 	
-	//List<Integer> calculator(int a,int b);
-	
-	int addition(int a,int b);
-	int subtraction(int a,int b);
-	int multiply(int a,int b);
+	@WebResult(name="MultiplyResult")
+	int multiply(@WebParam(name="IntegerInput_1")int a,@WebParam(name="IntegerInput_2")int b);
+
+	List<Integer> calculator(@WebParam(name="IntegerInput_1")int a,@WebParam(name="IntegerInput_2")int b);
 
 }
--- WevServiceFramework/SoapServiceTomcat/src/main/java/com/nostalgiaguy/soapservicetomcat/TestMethodImplementation.java
+++ WevServiceFramework/SoapServiceTomcat/src/main/java/com/nostalgiaguy/soapservicetomcat/TestMethodImplementation.java
@@ -2,54 +2,95 @@ package com.nostalgiaguy.soapservicetomcat;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.annotation.Resource;
 import javax.jws.WebService;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-@WebService(endpointInterface="com.nostalgiaguy.soapservicetomcat.TestMethod",
-portName="testMethodPort",
-serviceName="testMethod")
-public class TestMethodImplementation implements TestMethod{
+@WebService(endpointInterface = "com.nostalgiaguy.soapservicetomcat.TestMethod", portName = "testMethodPort", serviceName = "testMethod")
+public class TestMethodImplementation implements TestMethod {
 
-	public int addition(int a, int b) {
-		return a+b;
+	static Logger log = LogManager.getLogger(TestMethodImplementation.class
+			.getName());
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.nostalgiaguy.soapservicetomcat.TestMethod#addition(int, int)
+	 * 
+	 * addition service is using header authorization.
+	 */
+	public String addition(int a, int b) {
+		log.info("Entering into additon service");
+		boolean test = authorizationTest();
+		if (test) {
+			log.info("Valid user");
+			return Integer.toString(a + b);
+		} else {
+			log.info("Invalid user");
+			return "Invalid User";
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.nostalgiaguy.soapservicetomcat.TestMethod#subtraction(int, int)
+	 */
 	public int subtraction(int a, int b) {
-		return a-b;
+		return a - b;
 	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.nostalgiaguy.soapservicetomcat.TestMethod#multiply(int, int)
+	 */
 
 	public int multiply(int a, int b) {
-		return a*b;
+		return a * b;
 	}
 
-//	@Override
-//	public List<Integer> calculator(int a, int b) {
-//		List<Integer> result = new ArrayList<>();
-//		//result.add(new Add(a,b).getAddition());
-//		//result.add(new Sub(a,b).getSubtraction());
-//		result.add(a+b);
-//		result.add(a-b);
-//		result.add(a*b);
-//		return result;
-//	}
+	/*
+	 * (non-Javadoc)
+	 * @see com.nostalgiaguy.soapservicetomcat.TestMethod#calculator(int, int)
+	 */
+	public List<Integer> calculator(int a, int b) {
+		List<Integer> result = new ArrayList<>();
+		result.add(a + b);
+		result.add(a - b);
+		result.add(a * b);
+		return result;
+	}
 
-//	@Override
-//	public int multiply(int a, int b) {
-//
-//		return a*b;
-//	}
+	@Resource
+	private static WebServiceContext wsc;
+
+	/*
+	 * This method is validating username and password from header of the
+	 * request.
+	 */
+	public static boolean authorizationTest() {
+		MessageContext mc = wsc.getMessageContext();
+		Map requestHeader = (Map) mc.get(MessageContext.HTTP_REQUEST_HEADERS);
+		List userList = (List) requestHeader.get("Username");
+		List passList = (List) requestHeader.get("Password");
+		String username = "";
+		String password = "";
+		if (passList != null && userList != null) {
+			username = (String) userList.get(0);
+			password = (String) passList.get(0);
+		}
+		if ("shubham".equals(username) && "pathak".equals(password)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 }
-
-
-/*
- * @javax.jws.WebService(
-        serviceName = "CalculatorPull",
-        portName = "CalculatorPullSOAP",
-        targetNamespace = "com.nostalgiaguy",
-        wsdlLocation = "CalculatorPull.wsdl",
-        endpointInterface = "com.nostalgiaguy.CalculatorPull")
-        
-        
-        */
